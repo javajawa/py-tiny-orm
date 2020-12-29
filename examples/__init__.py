79b42ce8973d97a99d91cc9f518fe22d39fcb130
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2020-12-29 12:49:29 +0000

    Initial Commit

---
+++ examples/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# vim: fileencoding=utf-8 expandtab ts=4 nospell
+
+# SPDX-FileCopyrightText: 2020 Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
+#
+# SPDX-License-Identifier: BSD-2-Clause
